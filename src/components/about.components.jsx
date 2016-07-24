c236a1e82ea737c22153d514dd9bd74d900ab927
avi-jain <avijain97@ymail.com>
2016-07-24 21:56:06 +0530

    fisrt commit

---
+++ src/components/about.components.jsx
@@ -0,0 +1,76 @@
+var React = require('react');
+var TypeWriter = require('react-typewriter');
+
+var About = React.createClass({
+    
+    render : function(){
+
+        var aboutStyle={
+          background : '#5C978F' ,
+          height: ' 100vh'
+        };
+        var textStyle={
+          fontSize:'2.5em',
+          color:'#393939',
+          fontFamily:'Bungee Shade',
+          fontWeight:'bold',
+          marginTop:'10px'
+        };
+        var anotherTextStyle={
+          fontSize:'3.5em',
+          color:'#393939',
+          fontFamily:'Bungee Shade',
+          fontWeight:'bold',
+          marginTop:'30px'
+        };
+        var yetAnotherTextStyle={
+          fontSize:'2.5em',
+          color:'#393939',
+          fontFamily:'Bungee Shade',
+          fontWeight:'bold',
+          marginTop:'60px'
+        };
+        var socialStyle={
+          width:'40px',
+          height:'40px',
+          margin:'5px'
+        }
+        var logoStyle={
+          width:'100px',
+          height:'100px',
+          margin:'6px'
+        }
+        var spanStyle={
+          float:'none', 
+          display:'block',
+          marginLeft:'auto',
+          marginRight:'auto'
+        }
+        return(
+           <div  id="about" style={aboutStyle}>
+            <div className='about container'>
+                <p className='text-center' style={textStyle}>I happen to know a bit about</p>
+                {/*Center this shit. Have to specify .png for logos.*/}
+                <span style={spanStyle}>
+                {/*Add links to the logos.Add tooltips and accessibility options*/}
+                <img src="./../public/img/HTML5_Logo.png" style={logoStyle} />
+                <img src="./../public/img/css3logo.png" style={logoStyle} />
+                <img src="./../public/img/js_logo.png" style={logoStyle} />
+                <img src="./../public/img/bootstrap_logo.png" style={logoStyle} />
+                <img src="./../public/img/react-logo.png" style={logoStyle} />
+                <img src="./../public/img/nodejs-logo.png" style={logoStyle} />
+                </span>  
+                <p className='text-center' style={anotherTextStyle}> Building something with React? I could help.</p>
+                <p className='text-center' style={yetAnotherTextStyle}> Reach out to me on </p>
+                <span className=''>{/*Center this shit.Providing cdns(links) will reduce load time.*/}
+                <a href><img src="./../public/img/Tinder-Flame.png" style={socialStyle} className="tinder" /></a>
+                <a href><img src="./../public/img/trulymadly-logo.png" style={socialStyle} className="hinge"   /></a>
+                <a href><img src="./../public/img/hitch_logo.png" style={socialStyle} className="hitch"   /></a>
+                </span>  
+            </div>
+           </div>
+       );
+   } 
+});
+
+module.exports = About;
---
+++ src/components/home.components.jsx
@@ -0,0 +1,41 @@
+var React = require('react');
+var TypeWriter = require('react-typewriter');
+
+var HomePage = React.createClass({
+    showNextPage : function(){
+      var element = document.getElementByID("about");
+      element.scrollIntoView();
+    },
+    render : function(){
+        var homeStyle={
+          background : '#DE8642',
+          height: '100vh'
+        };
+        var headingStyle={
+          color: '#4B4D52',
+          fontSize:'100px',
+          fontFamily:'Pacifico' 
+        };
+        var contentStyle={
+          color:'#393939',
+          fontSize:'40px',
+          fontFamily:'Pacifico'
+        }
+        return(
+           <div style={homeStyle}>
+            <div className='container'>
+              <h1 className='hello text-center' style={headingStyle}>hello</h1>
+              <TypeWriter typing={1} onTypingEnd={this.showSkills}>
+                <p style={contentStyle}>Welcome to my github portfolio.<br />
+                When I'm not busy binge watching shows, gaming, reading, copying college assignments
+                or simply whiling time away on the internet...
+                <br/>
+                I code :)</p>
+              </TypeWriter>
+            </div>
+           </div>
+       );
+   } 
+});
+
+module.exports = HomePage;
